6aa690d1348c691ac19c44dfdc347f74680f22d8
Mkaell <mrohacevich@yandex.ru>
2022-05-30 17:39:26 +0300

    fixed problems in the workspace

--- react-testing/src/App.js
+++ react-testing/src/App.js
@@ -1,5 +1,3 @@
-import { useEffect, useState } from "react";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import AppRouter from "./router/AppRouter";
 
